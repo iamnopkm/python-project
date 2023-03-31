03d194ea46aaa087c3c13704e70fc199778ecaf3
Ho Quang Anh ID: BI12-017 <mebum10@gmail.com>
2023-03-31 11:10:18 +0700

    gui

---
+++ gui/Frame.py
@@ -0,0 +1,7 @@
+from tkinter import Frame as tkFrame
+from .Widget import Widget
+
+class Frame(tkFrame):
+    def __init__(self, master, width: int = 400, height: int = 400, background: str = "white"):
+        
+        super().__init__(master, width = width, height = height, background = background)
