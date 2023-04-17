cc2d79148411a61290af01781991912b46500b8a
Ho Quang Anh ID: BI12-017 <mebum10@gmail.com>
2023-04-17 20:32:31 +0700

    update Grid_rowconfigure

--- OperationUI/OperationCommandGUI.py
+++ OperationUI/OperationCommandGUI.py
@@ -697,7 +697,7 @@ class CustomerOperation:
                 edit_options_window.title("Edit Options")
                 edit_options_window.configure(bg="#FFCAD4")
                 edit_options_window.grid_columnconfigure((0,2), weight=1)
-                edit_options_window.grid_rowconfigure((0, 11), weight=1)
+                # edit_options_window.grid_rowconfigure((0, 11), weight=1)
 
                 edit_name = Button(
                     edit_options_window, 
